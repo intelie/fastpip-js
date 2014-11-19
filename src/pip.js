afde84112c227be24bd7a3cae6a546cf0f19c809
Breno Ferreira <bferreira@windowslive.com>
2014-11-19 15:48:55 -0200

    PIP implementation
    Perceptually Important Points algorithm

---
+++ src/pip.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var PipHeap = function(distance){
+  this.distance = distance;
+  this.heap = [];
+};
+
+PipHeap.prototype.add = function(value){
+  this.heap.push(value);
+};
+
+PipHeap.prototype.removeMin = function() {
+  var value = +Infinity;
+  var index = 0;
+  for (var i = 1; i < this.heap.length - 1; i++) {
+    var newValue = this.distance(this.heap[i - 1], this.heap[i], this.heap[i + 1]);
+    if (newValue < value) {
+      value = newValue;
+      index = i;
+    }
+  }
+  return this.heap.splice(index, 1)[0];
+};
+
+PipHeap.prototype.size = function() {
+  return this.heap.length;
+};
+
+var createVerticalDistance = function(x, y){
+  return function(a, b, c){
+    return verticalDistance(a, b, c, x, y);
+  };
+};
+
+var verticalDistance = function(a, b, c, x, y){
+  return Math.abs(((a.get(y) + (c.get(y) - a.get(y)) * (b.get(x) - a.get(x)) /
+                          (c.get(x) - a.get(x)) - b.get(y))) *
+                (c.get(x) - a.get(x)));
+};
+
+var pip = function(data, k, xProp, yProp) {
+  var heap = new PipHeap(createVerticalDistance(xProp, yProp));
+
+  for(var i = 0; i < data.count(); i++) {
+    heap.add(data.get(i));
+    if (heap.size() <= k)
+      continue;
+
+    heap.removeMin();
+  }
+  return heap.heap;
+};
+
+if(window){
+  window.pip = pip;
+}
+else if(module && module.exports){
+  module.exports = pip;
+}
+
